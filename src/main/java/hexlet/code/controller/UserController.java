53943a13704988d7ee838b7a536315cd859dee5c
ArturStimbiris <artur.stimbirys@gmail.com>
2025-09-09 23:55:55 +0300

    upd UserController2

--- src/main/java/hexlet/code/controller/UserController.java
+++ src/main/java/hexlet/code/controller/UserController.java
@@ -8,7 +8,6 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -48,14 +47,12 @@ public class UserController {
     }
 
     @PutMapping("/{id}")
-    @PreAuthorize("authentication.principal.username == @userService.findEmailById(#id)")
     public UserDTO update(@PathVariable Long id, @Valid @RequestBody UserUpdateDTO userUpdateDTO) {
         return userService.update(id, userUpdateDTO);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PreAuthorize("authentication.principal.username == @userService.findEmailById(#id)")
     public void destroy(@PathVariable Long id) {
         userService.delete(id);
     }
