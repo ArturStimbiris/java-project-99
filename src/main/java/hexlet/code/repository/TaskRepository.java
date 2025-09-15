afdf98e927cad67bd933ddae59d2c4841a185519
ArturStimbiris <artur.stimbirys@gmail.com>
2025-09-16 00:46:27 +0300

    review2 fix4

--- src/main/java/hexlet/code/repository/TaskRepository.java
+++ src/main/java/hexlet/code/repository/TaskRepository.java
@@ -12,14 +12,14 @@ import java.util.List;
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
     @Query("SELECT t FROM Task t WHERE "
-                + "(:assigneeId IS NULL OR t.assignee.id = :assigneeId) AND "
-                + "(:status IS NULL OR t.taskStatus.slug = :status) AND "
-                + "(:labelId IS NULL OR EXISTS (SELECT l FROM t.labels l WHERE l.id = :labelId)) AND "
-                + "(:titleCont IS NULL OR LOWER(t.title) LIKE LOWER(CONCAT('%', :titleCont, '%')))")
-        List<Task> findByFilters(
-                @Param("assigneeId") Long assigneeId,
-                @Param("status") String status,
-                @Param("labelId") Long labelId,
-                @Param("titleCont") String titleCont
-        );
+            + "(:assigneeId IS NULL OR t.assignee.id = :assigneeId) AND "
+            + "(:status IS NULL OR t.taskStatus.slug = :status) AND "
+            + "(:labelId IS NULL OR EXISTS (SELECT l FROM t.labels l WHERE l.id = :labelId)) AND "
+            + "(:titleCont IS NULL OR LOWER(t.title) LIKE LOWER(CONCAT('%', CAST(:titleCont AS text), '%')))")
+    List<Task> findByFilters(
+            @Param("assigneeId") Long assigneeId,
+            @Param("status") String status,
+            @Param("labelId") Long labelId,
+            @Param("titleCont") String titleCont
+    );
 }
